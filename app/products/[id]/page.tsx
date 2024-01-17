3e748916d6b7aaa59e4236c80305178a3b46c9ee
Dimitar <dimitar.mandarliev@gmail.com>
2024-01-17 10:27:57 +0200

    added some explanation why we need types

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,6 +1,10 @@
 import { notFound } from "next/navigation";
 import React from "react";
 
+/**
+ * In TypeScript you need to specify the type of props you are going to send or it takes the default type defined tin @types/react. 
+ * If you dont want to specify any type then explicitly ask the component to expect state and props of ‘any’ type.
+ */
 type Props = {
   params: {
     id: string;
